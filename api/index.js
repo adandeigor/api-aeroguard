c04919dbb494bcab6e3f9f430d41dd67b0d39aaa
ben ephraim <benagbannon@gmail.com>
2025-10-05 16:56:40 +0100

    Add 5

--- api/index.js
+++ api/index.js
@@ -1,21 +1,4 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import predictRouter from './api/predict.js';
-import alertRouter from './api/alerts.js';
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-// // 🔹 routes
-// app.use('/api/predict', predictRouter);
-// app.use('/api/alerts', alertRouter);
-
-
-app.get('/', (req, res) => {
-  res.send('Air-Qualify Backend is running');
-});
-
-
-export default app;
+// /api/index.js
+export default function handler(req, res) {
+  res.status(200).send('Air-Qualify Backend is running');
+}
